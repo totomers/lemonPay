12ac5720d301fd3fca6dea94c05d2b266fcbb5f3
Tomer Eyal <tomere@moveo.co.il>
2022-06-01 11:55:26 +0300

    changed getBusinessDetails

--- src/services/business-service/handlers/getBusinessDetails.handler.ts
+++ src/services/business-service/handlers/getBusinessDetails.handler.ts
@@ -5,11 +5,17 @@ export async function getBusinessDetailsHandler(params: { _id: string }) {
   try {
     await connectToDatabase();
     const { _id } = params;
-    const business = await Business.findById(_id).select({
-      createdAt: 0,
-      updatedAt: 0,
-      __v: 0,
-    });
+    const business = await Business.findById(_id)
+      .select({
+        createdAt: 0,
+        updatedAt: 0,
+        __v: 0,
+      })
+      .populate({
+        path: 'businessAdmin',
+        model: 'user',
+        select: { createdAt: 0, updatedAt: 0, __v: 0, businesses: 0 },
+      });
 
     return business;
   } catch (error) {
