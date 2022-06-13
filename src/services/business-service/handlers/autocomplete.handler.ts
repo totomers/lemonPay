334a528097e692bd2135585a78473b734a3d4b0c
Tomer Eyal <tomere@moveo.co.il>
2022-06-13 17:24:07 +0300

    added autocomplete endpoint

--- src/services/business-service/handlers/autocomplete.handler.ts
+++ src/services/business-service/handlers/autocomplete.handler.ts
@@ -2,36 +2,18 @@ import { connectToDatabase } from 'src/database/db';
 import https from 'https';
 import { Business } from 'src/database/models/business';
 import { CONFIG } from 'src/config';
+import axios from 'axios';
 
 export async function autocompleteHandler(params: { name: string }) {
   try {
     const { name } = params;
     const kvkUrl = `https://developers.kvk.nl/test/api/v1/zoeken?handelsnaam=${name}&pagina=1&aantal=50`;
     const kvkToken = CONFIG.KVK.TOKEN;
-    const options = {
-      hostname: kvkUrl,
-      port: 443,
-      //   path: '/todos',
-      method: 'GET',
-      headers: {
-        Authorization: ` Bearer ${kvkToken}`,
-      },
-    };
 
-    const req = https.request(options, (res) => {
-      console.log(`statusCode: ${res.statusCode}`);
+    const results = await axios.get(kvkUrl);
+    console.log(results);
 
-      res.on('data', (d) => {
-        console.log('data received', d);
-        process.stdout.write(d);
-      });
-    });
-
-    req.on('error', (error) => {
-      console.error(error);
-    });
-
-    req.end();
+    return results.data;
   } catch (error) {
     return error;
   }
